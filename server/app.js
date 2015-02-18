095059aa97d6b3eab836154638daf778e115b5bd
Edouard Durieux <edouard.durieux@gmail.com>
2015-02-18 16:24:00 +0100

    Changing jade to swig

--- server/app.js
+++ server/app.js
@@ -1,22 +1,31 @@
 // Module dependencies.
 
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var swig = require('swig');
 
 var app = express();
 
 // all environments
+
+app.engine('html', swig.renderFile);
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
+
 app.use(express.favicon());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+
 app.use(app.router);
+
+app.set('view cache', false);
+swig.setDefaults({ cache: false });
 
 // development only
 if ('development' == app.get('env')) {
