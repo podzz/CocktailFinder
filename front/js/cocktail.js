11e3eae4415191c49135282eda66c72aaf5cc196
François BOITEUX <francois.boiteux@epita.fr>
2015-03-10 04:32:45 +0100

    Add clean button

--- front/js/cocktail.js
+++ front/js/cocktail.js
@@ -1,34 +1,41 @@
-$(function(){
-   $("#runapp").cycle({
-       fx: "scrollHorz",
-       slides: "> div",
-       timeout: 0,
-       speed: 1000,
-       log: false
-   });
+$(function () {
+    $("#runapp").cycle({
+        fx: "scrollHorz",
+        slides: "> div",
+        timeout: 0,
+        speed: 1000,
+        log: false
+    });
 
-    $(".gotoapp").on("click", function() {
+    $(".gotoapp").on("click", function () {
         $("#runapp").cycle("next");
         $(".background").addClass("background-transition");
     });
 
-    $(".backtohome").on("click", function() {
+    $(".backtohome").on("click", function () {
         $("#runapp").cycle("prev");
         $(".background").removeClass("background-transition");
 
     });
 
     $("body").removeClass("preload");
+
 });
 
-function delete_button(item) {
-    var buttonToDelete = "#itemli" + item;
+function delete_all_button() {
+    var listItemLi = $("li[id*='itemli']");
+    for (i = 0; i < listItemLi.length; i++)
+            delete_button(i + 1);
+    $("ul[name='excludes']").hide("slow");
+}
+
+function delete_button(id) {
+    var buttonToDelete = "#itemli" + id;
     $(buttonToDelete).hide("slow");
     var listItemLi = $("li[id*='itemli']");
     if (listItemLi.length <= 1)
         $("ul[name='excludes']").hide("slow");
     setTimeout(function () {
-
         $(buttonToDelete).remove();
     }, 400);
 }
@@ -43,10 +50,9 @@ function add_button(item) {
         i = i + 1;
         myElem = document.getElementById('itemli' + i);
     }
-
     iDiv.id = '';
     iDiv.className = '';
-    iDiv.innerHTML = '<li id="itemli'+ i + '"> \
+    iDiv.innerHTML = '<li id="itemli' + i + '"> \
                         <button type="button" style="display:none;" id="item' + i + '" onClick="delete_button(' + i + ')" class="btn btn-default"> \
                             <span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Item ' + i + ' \
                         </button> \
