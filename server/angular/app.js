592d12eaff482bb0f7ee543c5d9022bdf841561b
Edouard Durieux <edouard.durieux@gmail.com>
2015-03-15 13:05:27 +0100

    Added async call for missing query on angular

--- server/angular/app.js
+++ server/angular/app.js
@@ -38,8 +38,10 @@
 		};
 		// Append an ingredient to the missing list
 		this.addMissing = function(ingredient) {
+			console.log(this.missing);
             if (this.missing.indexOf(ingredient) == -1)
 			    this.missing.push(ingredient);
+			this.reloadData();
 		}
 		// Remove an ingredient ID from the missing list
 		this.removeMissing = function(ingredient) {
@@ -48,6 +50,24 @@
 	 			   this.missing.splice(i, 1);
 		        }
 	    	}
+		}
+		this.reloadData = function() {
+			var route = "/cocktails/missing/";
+
+	        for (var i = 0; i < this.missing.length; ++i) {
+        		if (i == idTab.length - 1) {
+					route += this.missing[i].id;
+				} else {
+					route += this.missing[i].id + ',';
+				}
+			}
+
+			$http.get(route).success(function(data){
+				that.currentCocktail = data.cocktails[0];
+				// Data fetched from server
+				that.data = data;
+			}
+		});
 		}
 	}]);
 })();
