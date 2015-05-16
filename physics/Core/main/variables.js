b1254e830a4840d04853c4577eeae33625ae0ba9
François Boiteux <francois.boiteux@epita.fr>
2015-05-16 20:27:01 +0200

    [FRONT] Update css style

--- physics/Core/main/variables.js
+++ physics/Core/main/variables.js
@@ -7,7 +7,7 @@ var glassLineColor = 0xE0E0E0;
 var glassLineStrength = 4;
 
 var displayLineColor = 0x000000;
-var displayFillColor = 0x00b7e2 ;
+var displayFillColor = 0x35383A;
 var displayLineStrength = 3;
 
 var glassScale = 2;
