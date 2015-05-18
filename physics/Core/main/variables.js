39a1d92a4ea58d3a96a11b6e41501480945661ad
François Boiteux <francois.boiteux@epita.fr>
2015-05-18 02:55:51 +0200

    [FRONT] Add better css, more luxurious

--- physics/Core/main/variables.js
+++ physics/Core/main/variables.js
@@ -7,7 +7,7 @@ var glassLineColor = 0xE0E0E0;
 var glassLineStrength = 4;
 
 var displayLineColor = 0x000000;
-var displayFillColor = 0x35383A;
+var displayFillColor = 0x4489ba;
 var displayLineStrength = 3;
 
 var glassScale = 2;
