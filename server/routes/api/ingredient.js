7291d451ab56433d6c8f927686ec14b97642e68f
Edouard Durieux <edouard.durieux@gmail.com>
2015-07-06 18:32:05 +0200

    Added new server CORS handling, and start of an angularJS CRUD backend

--- server/routes/api/ingredient.js
+++ server/routes/api/ingredient.js
@@ -2,12 +2,15 @@ var controllerIngredient = require('../../controller/controllerIngredient');
 
 exports.getIngredients = function (req, res, next) {
     controllerIngredient.getIngredients(function (err, result) {
+            res.set("Access-Control-Allow-Origin", "*");
+
         res.json(result);
     });
 };
 
 exports.addIngredient = function (req, res, next) {
     controllerIngredient.addIngredient(req.body, function (err, result) {
+            res.set("Access-Control-Allow-Origin", "*");
         res.json(result);
     });
 };
@@ -16,12 +19,14 @@ exports.getIngredientById = function(req, res, next) {
     var param = req.params.id;
 
     controllerIngredient.getIngredientById(param, function (err, result) {
+            res.set("Access-Control-Allow-Origin", "*");
         res.json(result);
     });
 };
 
 exports.putIngredientById = function(req, res, next) {
     controllerIngredient.putIngredientById(req.params.id, req.body, function (err, result) {
+            res.set("Access-Control-Allow-Origin", "*");
         res.json(result);
     });
 };
@@ -30,6 +35,7 @@ exports.deleteIngredientById = function(req, res, next) {
     var param = req.params.id;
 
     controllerIngredient.deleteIngredientById(param, function (err, result) {
+            res.set("Access-Control-Allow-Origin", "*");
         res.json(result);
     });
 };
