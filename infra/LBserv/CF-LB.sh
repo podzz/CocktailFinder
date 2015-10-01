12a93e100ae79bd049a02b68600397d434b1f9ca
François Boiteux <francois.boiteux@epita.fr>
2015-10-01 17:05:41 +0200

    [NGINX] Add vagrant deploy for nginx

--- infra/LBserv/CF-LB.sh
+++ infra/LBserv/CF-LB.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y nginx
+sudo apt-get install -y nginx git curl npm
+
+git clone https://github.com/Flasheur111/CocktailFinder.git
+#Replace nginx configuration
 cp /vagrant/default /etc/nginx/sites-enabled/
 sudo service nginx start
+
+
