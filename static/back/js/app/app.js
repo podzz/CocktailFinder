f2ff417f60c7ee1cd04a9e7546c556129aaf4969
Edouard Durieux <edouard.durieux@gmail.com>
2015-10-03 18:52:00 +0200

    Updated backend

--- static/back/js/app/app.js
+++ static/back/js/app/app.js
@@ -3,59 +3,59 @@ angular.module('adminApp', ['ui.router', 'ngResource', 'adminApp.controllers', '
 angular.module('adminApp').config(function($stateProvider, $httpProvider){
     $stateProvider.state('ingredients',{
         url:'/ingredients',
-        templateUrl:'admin/partials/ingredients.html',
+        templateUrl:'static/back/html/partials/ingredients.html',
         controller:'IngredientListController'
     }).state('viewIngredient',{
        url:'/ingredients/:id/view',
-       templateUrl:'admin/partials/ingredient-view.html',
+       templateUrl:'static/back/html/partials/ingredient-view.html',
        controller:'IngredientViewController'
     }).state('newIngredient',{
         url:'/ingredients/new',
-        templateUrl:'admin/partials/ingredient-add.html',
+        templateUrl:'static/back/html/partials/ingredient-add.html',
         controller:'IngredientCreateController'
     }).state('editIngredient',{
         url:'/ingredients/:id/edit',
-        templateUrl:'admin/partials/ingredient-edit.html',
+        templateUrl:'static/back/html/partials/ingredient-edit.html',
         controller:'IngredientEditController'
     }).state('glasses',{
         url:'/glasses',
-        templateUrl:'admin/partials/glasses.html',
+        templateUrl:'static/back/html/partials/glasses.html',
         controller:'GlassListController'
     }).state('viewGlass',{
         url:'/glasses/:id/view',
-        templateUrl:'admin/partials/glass-view.html',
+        templateUrl:'static/back/html/partials/glass-view.html',
         controller:'GlassViewController'
     }).state('newGlass',{
         url:'/glasses/new',
-        templateUrl:'admin/partials/glass-add.html',
+        templateUrl:'static/back/html/partials/glass-add.html',
         controller:'GlassCreateController'
     }).state('editGlass',{
         url:'/glasses/:id/edit',
-        templateUrl:'admin/partials/glass-edit.html',
+        templateUrl:'static/back/html/partials/glass-edit.html',
         controller:'GlassEditController'
     }).state('recipes',{
         url:'/recipes',
-        templateUrl:'admin/partials/recipes.html',
+        templateUrl:'static/back/html/partials/recipes.html',
         controller:'RecipeListController'
     }).state('viewRecipe',{
         url:'/recipes/:id/view',
-        templateUrl:'admin/partials/recipe-view.html',
+        templateUrl:'static/back/html/partials/recipe-view.html',
         controller:'RecipeViewController'
     }).state('newRecipe',{
         url:'/recipes/new',
-        templateUrl:'admin/partials/recipe-add.html',
+        templateUrl:'static/back/html/partials/recipe-add.html',
         controller:'RecipeCreateController'
     }).state('editRecipe',{
         url:'/recipes/:id/edit',
-        templateUrl:'admin/partials/recipe-edit.html',
+        templateUrl:'static/back/html/partials/recipe-edit.html',
         controller:'RecipeEditController'
     }).state('verify', {
         url:'/verify',
-        templateUrl: 'admin/partials/verify.html',
+        templateUrl: 'static/back/html/partials/verify.html',
         controller: 'verifyController'
     }).state('unity',{
         url:'/unities',
-        templateUrl:'admin/partials/unities.html',
+        templateUrl:'static/back/html/partials/unities.html',
         controller:'UnitiesController'
     });
 }).run(function($state){
