5bd974426219d0d09c9bc62749eac8b63e86356a
Edouard Durieux <edouard.durieux@gmail.com>
2015-10-26 16:26:17 +0100

    Code cleaning on backoffice

--- backoffice/backoffice.js
+++ backoffice/backoffice.js
@@ -5,33 +5,23 @@
 var express         = require('express');
 var http            = require('http');
 var path            = require('path');
-var fs              = require('fs');
 
 // Middlewares
 var bodyParser      = require('body-parser');
 var morgan          = require('morgan');
 var methodOverride  = require('method-override');
 var errorhandler    = require('errorhandler');
-var nodeCache       = require('node-cache');
 
 // CocktailFd Route module
 var routes          = require('./routes');
 
 var app             = express();
-var myCache         = new nodeCache({stdTTL: 600, checkperiod: 660});
 
 // ---------------------------------
 // ENV setup
 // ---------------------------------
 
 app.set('port', process.env.PORT || 3000);
-
-// Static assets directory path parameter
-app.use(express.static(path.join(__dirname, 'angular')));
-app.use(express.static(path.join(__dirname, '../front')));
-app.use(express.static(path.join(__dirname, '../physics')));
-app.use('/admin', express.static(path.join(__dirname, '../admin')));
-
 
 // CORS Control for remote API cases
 var allowCrossDomain = function(req, res, next) {
@@ -68,8 +58,6 @@ if ('development' == app.get('env')) {
 // ---------------------------------
 // Routes
 // ---------------------------------
-
-// App routes
 
 // Bootstrap/Install routes
 app.get('/bdd/rank/ingredients',	routes.bdd.rankIngredients);
@@ -113,43 +101,13 @@ app.get('/api/glasses/:id',         routes.api.glass.getGlassById);
 app.put('/api/glasses/:id',         routes.api.glass.putGlassById);
 app.delete('/api/glasses/:id',      routes.api.glass.deleteGlassById);
 
-// Old
-app.get('/api/cocktails',           routes.api.findCocktails);
-app.get('/api/cocktails/:ids', 		  routes.api.findCocktails);
-app.get('/api/ingredients/setColor/:ingredient/:color', routes.api.setColor); // PRODUCTION
-app.get('/api/ingredients/setOpacity/:ingredient/:opacity', routes.api.setOpacity);  // PRODUCTION
-app.get('/api/missing', 			      routes.api.findCocktailsByMissingIds); // PRODUCTION
-app.get('/api/missing/', 			      routes.api.findCocktailsByMissingIds); // PRODUCTION
-app.get('/api/missing/:array', function(req, res, next){
-    value = myCache.get(req.params.array);
-    if (value == undefined){
-      next();
-    } else {
-      req.cached = "FROM CACHE";
-      res.json(value);
-    }
-},
-routes.api.findCocktailsByMissingIds,
-function(req, res){
-    var constructArray = null;
-    if (req.params.array) {
-        constructArray = req.params.array.split(',');
-    }
-    if (constructArray.length <= 15) {
-        req.cached = "CACHING";
-        myCache.set(req.params.array, res.locals.result, 10000);
-    } else {
-        req.cached = "TOO LONG";
-    }
-});
-
 // ---------------------------------
 // Server deployment
 // ---------------------------------
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('-------------------------------------');
-  console.log('Cocktail Finder');
+  console.log('Cocktail Finder BackOffice APIs');
   console.log('-------------------------------------');
   console.log('Server running');
   console.log('Server listening @ http://localhost:%d/', app.get('port'));
--- backoffice/routes/index.js
+++ backoffice/routes/index.js
@@ -1,7 +1,4 @@
-//exports.logger 			= require('./logger');
-
 exports.bing 			= require('./bing');
-
 exports.api            	= require('./api');
 exports.api.ingredient 	= require('./api/ingredient');
 exports.api.glass      	= require('./api/glass')
