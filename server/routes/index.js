dfd8597206360526b5807f6947cfd9ed5e31f5f9
Edouard Durieux <edouard.durieux@gmail.com>
2015-10-26 11:14:55 +0100

    Deleting old server codebase

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-//exports.logger 			= require('./logger');
-
-exports.bing 			= require('./bing');
-
-exports.api            	= require('./api');
-exports.api.ingredient 	= require('./api/ingredient');
-exports.api.glass      	= require('./api/glass')
-exports.api.recipe     	= require('./api/recipe');
-exports.bdd 			= require('./bdd');
